80a1a22e23dcacd38e841ac07fdaa988c6fc7522
tianting <tianting@cqfmbank.com>
2018-10-11 15:19:43 +0800

    update

--- template/config.js
+++ template/config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    "port": 3000,
-    "domain": "http://localhost:3000",
-    "staticURL": "//localhost:3000"
+    port: 3000,
+    domain: "http://localhost:3000",
+    staticURL: "//localhost:3000",
+    globalJs: {
+        reactRoute: '',
+    }
 }
