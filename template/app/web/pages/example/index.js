93c48fa2d018a51d4f8fdadbf54c9bd35811663f
tianting <tianting@cqfmbank.com>
2019-04-16 10:35:15 +0800

    update

--- template/app/web/pages/example/index.js
+++ template/app/web/pages/example/index.js
@@ -4,11 +4,14 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
+import fastClick from "react-fastclick";
 
 import rootReducer from './reducer';
 import Router from './router';
 
 import '@common/css/global.scss';
+
+fastClick();
 
 const store = createStore(
     rootReducer,
