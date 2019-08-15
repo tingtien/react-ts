b565e3bbdfb231f2c0692331832a83a8569fa9d6
tianting <tianting@cqfmbank.com>
2019-08-15 18:24:32 +0800

    update

--- template/app/server/filter.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-// Example
-MI.get(/^\/example\/[^\/]+|\/$/, (req, res) => {
-    res.render('index');
-});
---
+++ template/app/server/router.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = {
+  'GET /^\/example\/[^\/]+|\/$/ R': 'index'
+}
