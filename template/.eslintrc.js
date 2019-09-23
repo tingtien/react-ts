436c5cd56d9bcf29302482e9faba220da20a7e99
tianting <tianting@cqfmbank.com>
2019-09-23 15:09:52 +0800

    update lint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,46 +1,32 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
-    },
-    // "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "experimentalObjectRestSpread": true,
-            "jsx": true
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true
         },
-        "sourceType": "module"
+        sourceType: "module"
     },
-    "parser": "babel-eslint",
-    "plugins": [
-        "react"
+    extends: [
+        "standard",
+        "plugin:prettier/recommended",
+        "plugin:react/recommended"
     ],
-    "rules": {
-        "semi": ["error", "always"],
-        "no-empty": 0,
-        "comma-dangle": 0,
-        "no-unused-vars": 0,
-        "no-console": 0,
-        "no-const-assign": 2,
-        "no-dupe-class-members": 2,
-        "no-duplicate-case": 2,
-        "no-extra-parens": [2, "functions"],
-        "no-self-compare": 2,
-        "accessor-pairs": 2,
-        "comma-spacing": [2, {
-            "before": false,
-            "after": true
-        }],
-        "constructor-super": 2,
-        "new-cap": [2, {
-            "newIsCap": false,
-            "capIsNew": false
-        }],
-        "new-parens": 2,
-        "no-array-constructor": 2,
-        "no-class-assign": 2,
-        "no-cond-assign": 2
+    plugins: ["react", "prettier"],
+    rules: {
+        "prettier/prettier": 2,
+        "react/prop-types": 0,
+        "react/display-name": 0,
+        "eqeqeq": 0,
+        // "no-undef": 0,
+        "camelcase": 0
+    },
+    globals: {
+        MI: "readonly"
+    },
+    settings: {
+        "react": {
+            "version": "detect", // React version. "detect" automatically picks the version you 
+        }
     }
 };
