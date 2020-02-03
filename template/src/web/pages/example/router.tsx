64c670b64633215996d64da43c942a70e75804af
tingtien <tianting@cqfmbank.com>
2020-02-03 13:19:43 +0800

    update

--- template/src/web/pages/example/router.tsx
+++ template/src/web/pages/example/router.tsx
@@ -5,32 +5,14 @@ import { Switch, Route, RouteComponentProps } from "react-router-dom";
 import NotFound from "@common/example/404";
 
 export default class ModalSwitch extends Component<RouteComponentProps> {
-  private previousLocation: any;
-
   constructor(props: RouteComponentProps) {
     super(props);
-    this.previousLocation = this.props.location;
-  }
-
-  componentDidUpdate(nextProps: RouteComponentProps) {
-    const { location } = this.props;
-    if (
-      nextProps.history.action !== "POP" &&
-      (!location.state || !location.state.modal)
-    ) {
-      this.previousLocation = nextProps.history.location;
-    }
   }
 
   render() {
     const { location } = this.props;
-    const isModal = !!(
-      location.state &&
-      location.state.modal &&
-      this.previousLocation !== location
-    ); // not initial render
     return (
-      <Switch location={isModal ? this.previousLocation : location}>
+      <Switch location={location}>
         {routes &&
           routes.map((item, index) => {
             return (
